4d15bc451eb31d895fd16ea63191ed6f41997a69
markjake <markjakearzadon@gmail.com>
2025-03-22 12:01:48 +0800

    ..

--- app/draw.tsx
+++ app/draw.tsx
@@ -1,38 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text } from "react-native";
 import { Canvas, Circle, useCanvasRef } from "@shopify/react-native-skia";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 
-const Demo = () => {
+import { useSharedValue } from "react-native-reanimated";
+
+// canvas
+const C = () => {
   const ref = useCanvasRef();
-  useEffect(() => {
-    setTimeout(() => {
-      // you can pass an optional rectangle
-      // to only save part of the image
-      const image = ref.current?.makeImageSnapshot();
-      if (image) {
-        // you can use image in an <Image> component
-        // Or save to file using encodeToBytes -> Uint8Array
-        const bytes = image.encodeToBytes();
-        console.log({ bytes });
-      }
-    }, 1000);
-  });
+  const position = useSharedValue({ x: 0, y: 0 });
+  const onLeft = useSharedValue(false);
+
+  const panGesture = Gesture.Pan()
+    .onUpdate(({ x, y }) => {
+      position.value = { x, y };
+      console.log(position.value);
+    })
+    .onEnd();
+
   return (
-    <Canvas style={{ flex: 1 }} ref={ref}>
-      <Circle r={128} cx={128} cy={128} color="red" />
-    </Canvas>
+    <GestureDetector gesture={panGesture}>
+      <Canvas ref={ref} style={{ flex: 1, backgroundColor: "white" }}>
+        <Circle cx={100} cy={100} r={10} strokeWidth={2} color="red" />
+      </Canvas>
+    </GestureDetector>
   );
 };
 
 const draw = () => {
   return (
-    <SafeAreaView className="w-full h-full bg-backgroundColor">
-      <View className="h-full items-center mt-14">
-        <Text className="text-chiggaYellow text-2xl">Write & Draw Graphs</Text>
-        <Demo />
-      </View>
-    </SafeAreaView>
+    <GestureHandlerRootView>
+      <SafeAreaView className="w-full h-full bg-backgroundColor p-5">
+        <Text className="text-chiggaYellow text-center text-2xl">
+          Write & Draw Graphs
+        </Text>
+        <C />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
 
